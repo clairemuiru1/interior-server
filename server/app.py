a2626476bb2e21c5aaf88b190ec4de8a1c6c2ded
clairemuiru1 <muiruclaire@gmail.com>
2024-08-26 16:14:34 +0300

    "Create address"

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ from flask_bcrypt import Bcrypt
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, validators, IntegerField
 from sqlalchemy.exc import IntegrityError
-from models import db, User
+from models import db, User, Address
 from flask_cors import CORS
 from flask_restful import reqparse
 import jwt
@@ -130,6 +130,30 @@ def checksession():
         return user.to_dict(), 200
     
     return {}, 401 
+
+@app.route('/address', methods=['POST'])
+def create_address():
+    data = request.get_json()
+
+    # Validate incoming data
+    required_fields = ['user_id', 'street', 'city', 'state', 'zip_code', 'country']
+    if not all(field in data for field in required_fields):
+        return jsonify({'error': 'Missing required fields'}), 400
+
+    new_address = Address(
+        user_id=data['user_id'],
+        street=data['street'],
+        city=data['city'],
+        state=data['state'],
+        zip_code=data['zip_code'],
+        country=data['country']
+    )
+
+    db.session.add(new_address)
+    db.session.commit()
+    
+    return jsonify({'message': 'Address created successfully', 'address': new_address.id}), 201
+
 
 if  __name__ == '__main__':
     app.run(port=5555, debug=True)
